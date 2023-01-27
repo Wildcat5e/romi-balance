afb279416f87fb1ee5684269188c385eeddb245c
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2023-01-27 17:21:05 -0500

    1st cut at autonomous balance

--- src/main/java/wildcat5e/commands/AutonomousBalance.java
+++ src/main/java/wildcat5e/commands/AutonomousBalance.java
@@ -3,29 +3,45 @@ package wildcat5e.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import wildcat5e.subsystems.Drivetrain;
 
+/**
+ * Assumes the Romi is placed in front of a trapezoid ramp. The Romi should climb the ramp and stop on top when
+ * balanced.
+ */
 public class AutonomousBalance extends CommandBase {
-    private Drivetrain drivetrain;
+    private final Drivetrain drivetrain;
+
+    private double pitch0       = Float.NaN;
+    private double currentPitch = Float.NaN;
+    private boolean hasClimbed = false;
 
     public AutonomousBalance(Drivetrain drivetrain) {
-        // Use addRequirements() here to declare subsystem dependencies.
         this.drivetrain = drivetrain;
     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {}
+    @Override public void execute() {
+        new DriveDistance(0.5, 1.0, drivetrain).execute();
+        currentPitch = drivetrain.gyro.getAngleY();
+        if (pitch0 == Float.NaN) {
+            pitch0 = currentPitch;
+        }
+        hasClimbed |= climbing();
+    }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {}
+    boolean climbing() {
+        return !balanced() && currentPitch > pitch0;
+    }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {}
+    boolean descending() {
+        return !balanced() && currentPitch < pitch0;
+    }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
+    boolean balanced() {
+        return Math.abs(currentPitch - pitch0) <= 5.0d;
+    }
+
+    @Override public void end(boolean interrupted) {}
+
+    @Override public boolean isFinished() {
+        return hasClimbed && balanced();
     }
 }
