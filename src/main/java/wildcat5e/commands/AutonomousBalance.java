3a3656b7e572f565c616ce5ab221fc1f3be7f9cc
mrsonic2 <paynegumbrill@gmail.com>
2023-01-27 17:58:12 -0500

    Autonomous Balance Command with no State Machine

--- src/main/java/wildcat5e/commands/AutonomousBalance.java
+++ src/main/java/wildcat5e/commands/AutonomousBalance.java
@@ -13,19 +13,26 @@ public class AutonomousBalance extends CommandBase {
     private double pitch0       = Float.NaN;
     private double currentPitch = Float.NaN;
     private boolean hasClimbed = false;
+    private boolean initPitch0 = true;
 
     public AutonomousBalance(Drivetrain drivetrain) {
         this.drivetrain = drivetrain;
     }
 
     @Override public void execute() {
-        new DriveDistance(0.5, 1.0, drivetrain).execute();
+        new DriveDistance(1, 5.0, drivetrain).execute();
         currentPitch = drivetrain.gyro.getAngleY();
-        if (pitch0 == Float.NaN) {
+        if (initPitch0) {
             pitch0 = currentPitch;
+            initPitch0 = false;
+            System.out.println(pitch0);
         }
         hasClimbed |= climbing();
+        System.out.println(hasClimbed + " climbing: " + climbing() + " currentPitch: " + (currentPitch) + " delta: " + (currentPitch-pitch0));
+    
     }
+
+   
 
     boolean climbing() {
         return !balanced() && currentPitch > pitch0;
@@ -36,7 +43,7 @@ public class AutonomousBalance extends CommandBase {
     }
 
     boolean balanced() {
-        return Math.abs(currentPitch - pitch0) <= 5.0d;
+        return Math.abs(currentPitch - pitch0) <= 8.0d;
     }
 
     @Override public void end(boolean interrupted) {}
