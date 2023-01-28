5424dfce6aae026b8c9a30b95b1f2e5a9d34e563
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2023-01-27 20:36:27 -0500

    Refactor to use state machine

--- src/main/java/wildcat5e/commands/AutonomousBalance.java
+++ src/main/java/wildcat5e/commands/AutonomousBalance.java
@@ -3,54 +3,67 @@ package wildcat5e.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import wildcat5e.subsystems.Drivetrain;
 
+import static wildcat5e.commands.AutonomousBalance.State.CHARGING;
+import static wildcat5e.commands.AutonomousBalance.State.CLIMBING;
+import static wildcat5e.commands.AutonomousBalance.State.DESCENDING;
+import static wildcat5e.commands.AutonomousBalance.State.FIELD;
+import static wildcat5e.commands.AutonomousBalance.State.INITIALIZING;
+
 /**
  * Assumes the Romi is placed in front of a trapezoid ramp. The Romi should climb the ramp and stop on top when
  * balanced.
  */
 public class AutonomousBalance extends CommandBase {
-    private final Drivetrain drivetrain;
+    enum State {INITIALIZING, FIELD, CLIMBING, CHARGING, DESCENDING}
 
-    private double pitch0       = Float.NaN;
-    private double currentPitch = Float.NaN;
-    private boolean hasClimbed = false;
-    private boolean initPitch0 = true;
+    final Drivetrain    drivetrain;
+    final DriveDistance forward;
+    final DriveDistance backward;
+
+    State  state = INITIALIZING;
+    double startingPitch;
+    double currentPitch;
 
     public AutonomousBalance(Drivetrain drivetrain) {
         this.drivetrain = drivetrain;
+        this.forward = new DriveDistance(1, 5.0, drivetrain);
+        this.backward = new DriveDistance(-1, 5.0, drivetrain);
     }
 
     @Override public void execute() {
-        new DriveDistance(1, 5.0, drivetrain).execute();
         currentPitch = drivetrain.gyro.getAngleY();
-        if (initPitch0) {
-            pitch0 = currentPitch;
-            initPitch0 = false;
-            System.out.println(pitch0);
-        }
-        hasClimbed |= climbing();
-     
-        System.out.println(String.format("%b climbing %b, currentPitch %2.2f, delta %2.2f", 
-                           hasClimbed, climbing(), currentPitch, currentPitch-pitch0));
-    
+        state = nextState();
+        System.out.printf("%s delta %2.2f%n", state, currentPitch - startingPitch);
     }
 
-   
+    State nextState() {
+        switch (state) {
+            case INITIALIZING:
+                startingPitch = currentPitch;
+                return FIELD;
+            case FIELD:
+                return balanced() ? FIELD : climbing() ? CLIMBING : DESCENDING;
+            case CLIMBING:
+                forward.execute();
+                return balanced() ? CHARGING : climbing() ? CLIMBING : DESCENDING;
+            case DESCENDING:
+                backward.execute();
+                return balanced() ? CHARGING : climbing() ? CLIMBING : DESCENDING;
+            case CHARGING:
+            default:
+                return balanced() ? CHARGING : climbing() ? CLIMBING : DESCENDING;
+        }
+    }
 
     boolean climbing() {
-        return !balanced() && currentPitch > pitch0;
-    }
-
-    boolean descending() {
-        return !balanced() && currentPitch < pitch0;
+        return !balanced() && currentPitch > startingPitch;
     }
 
     boolean balanced() {
-        return Math.abs(currentPitch - pitch0) <= 8.0d;
+        return Math.abs(currentPitch - startingPitch) <= 8.0d;
     }
 
-    @Override public void end(boolean interrupted) {}
-
     @Override public boolean isFinished() {
-        return hasClimbed && balanced();
+        return state == CHARGING;
     }
 }
